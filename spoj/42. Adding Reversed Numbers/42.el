c23c4909df5fb4f4622be06c5dbd6a03434cd9dc
Sebastian Arcila Valenzuela <sebastianarcila@gmail.com>
2011-10-07 19:50:58 -0500

    solved add and reverse problem, btw it is the slowest solution ;(

---
+++ spoj/42. Adding Reversed Numbers/42.el
@@ -0,0 +1,20 @@
+(defun reverseAnInteger (s)
+  (parse-integer (reverse (write-to-string s))))
+
+(defun solve ()
+  (let (
+        (a (reverseAnInteger (read)))
+        (b (reverseAnInteger (read)))
+        )
+    (print (reverseAnInteger (+ a b)))))
+
+(defun doit (cases)
+  (if (= cases 0)
+      NIL
+    (progn
+      (solve )
+      (doit (- cases 1))
+      )))
+
+(doit (read))
+      
