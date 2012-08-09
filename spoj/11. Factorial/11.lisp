3a0221be7417dad77d1fd96bfc90c0e691e08f43
Sebastian Arcila Valenzuela <sebastianarcila@gmail.com>
2012-08-08 23:14:03 -0500

    added an old time limit

---
+++ spoj/11. Factorial/11.lisp
@@ -0,0 +1,16 @@
+;; Time Limit
+(defun Z ( n acum  )
+  (if (= n 0)
+      acum
+    (let ((a (truncate (/ n 5)) ) )
+      ( Z  a  (+ acum a) )
+      )
+    ))
+(defun doit (m)
+  (if (= m 0)
+      0
+    (progn
+      (print (Z (read) 0) )
+      (doit (- m 1))
+      )))
+(doit (read))
