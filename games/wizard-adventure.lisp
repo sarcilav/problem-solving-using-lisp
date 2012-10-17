b14b8cc477b9782e424d3dff3ad6f1d999cbd800
Sebastian Arcila Valenzuela <sebastianarcila@gmail.com>
2012-10-17 17:45:16 -0400

    working repl game

--- games/wizard-adventure.lisp
+++ games/wizard-adventure.lisp
@@ -69,3 +69,45 @@
 
 (defun inventory ()
   (cons 'items- (objects-at 'body *objects* *object-locations*)))
+
+(defun game-repl ()
+  (let ((cmd (game-read)))
+    (unless (eq (car cmd) 'quit)
+      (game-print (game-eval cmd))
+      (game-repl))))
+
+(defun game-read ()
+  (let ((cmd (read-from-string ;; it is like read but from a string
+              (concatenate 'string "(" (read-line) ")"))))
+    (flet ((quote-it (x) ;; flet is like labels but labels is better
+             ;; for recursive functions
+             (list 'quote x)))
+      (cons (car cmd) (mapcar #'quote-it (cdr cmd)))))) ;; maps the
+;; rest of the cmd to quote-it and then append it to first element of
+;; cmd.
+
+(defparameter *allowed-commands* '(look walk pickup inventory))
+
+(defun game-eval (sexp)
+  (if (member (car sexp) *allowed-commands*)
+      (eval sexp) ;; still needs a way to handle #.{malicious
+      ;; call}. passed in  the params
+      '(i do not know that command.)))
+
+(defun tweak-text (lst caps lit)
+  (when lst
+    (let ((item (car lst))
+          (rest (cdr lst)))
+      (cond ((eq item #\space) (cons item (tweak-text rest caps lit)))
+            ((member item '(#\! #\? #\.)) (cons item (tweak-text rest t lit)))
+            ((eq item #\") (tweak-text rest caps (not lit)))
+            (lit (cons item (tweak-text rest nil lit)))
+            ((or caps lit) (cons (char-upcase item) (tweak-text rest nil lit)))
+            (t (cons (char-downcase item) (tweak-text rest nil nil)))))))
+
+(defun game-print (lst)
+  (princ (coerce (tweak-text (coerce (string-trim "() " (prin1-to-string lst))
+                                     'list)
+                             t nil)
+                 'string))
+  (fresh-line))
