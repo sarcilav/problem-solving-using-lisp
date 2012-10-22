3064584e3f28a21dd12cd61c9e7ca0840dad986b
Sebastian Arcila Valenzuela <sebastianarcila@gmail.com>
2012-10-22 14:27:59 -0400

    add graph util

---
+++ grapher/graph-util.lisp
@@ -0,0 +1,66 @@
+;; data samples
+(defparameter *wizard-nodes* '((living-room (you are in the living-room.
+                                             a wizard is snoring loudly on the couch.))
+                               (garden (you are in a beautiful garden.
+                                        there is a well in front of you.))
+                               (attic (you are in the attic. there
+                                       is a giant welding torch in tahe corner.))))
+(defparameter *wizard-edges* '((living-room (garden west door)
+                                (attic upstairs ladder))
+                               (garden (living-room east door))
+                               (attic (living-room downstairs ladder))))
+
+(defun dot-name (exp)
+  (substitute-if #\_ (complement #'alphanumericp) (prin1-to-string exp)))
+
+(defparameter *max-label-length* 30)
+
+(defun dot-label (exp)
+  (if exp
+      (let ((s (write-to-string exp :pretty nil)))
+        (if (> (length s) *max-label-length*)
+            (concatenate 'string (subseq s 0 (- *max-label-length* 3)) "...")
+            s))
+      ""))
+
+(defun nodes->dot (nodes)
+  (mapc (lambda (node) ;; mapc it is like mapcar but without the fact that
+          ;; mapcar returns the modified list
+          (fresh-line)
+          (princ (dot-name (car node)))
+          (princ "[label=\"")
+          (princ (dot-label node))
+          (princ "\"];"))
+        nodes))
+
+(defun edges->dot (edges)
+  (mapc (lambda (node)
+          (mapc (lambda (edge)
+                  (fresh-line)
+                  (princ (dot-name (car node)))
+                  (princ "->")
+                  (princ (dot-name (car edge)))
+                  (princ "[label=\"")
+                  (princ (dot-label (cdr edge)))
+                  (princ "\"];"))
+                (cdr node)))
+        edges))
+
+(defun graph->dot (nodes edges)
+  (princ "digraph{")
+  (nodes->dot nodes)
+  (edges->dot edges)
+  (princ "}"))
+
+(defun dot->png (fname thunk)
+  (with-open-file (*standard-output* ;; ???
+                   fname
+                   :direction :output ;; key params ???
+                   :if-exists :supersede)
+    (funcall thunk))  ;; thunk???
+  (sb-ext:run-program "/usr/local/bin/dot" `("-Tpng" "-O" ,fname))) ;; sb-ext instead of ext:shell
+
+(defun graph->png (fname nodes edges)
+  (dot->png fname
+            (lambda ()
+              (graph->dot nodes edges))))
